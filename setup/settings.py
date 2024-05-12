1f001f6a1ac592aec6a11eb3a8bc22a4bbb020c5
Gabriel Mello <gabrielpontemello1@gmail.com>
2024-05-12 19:12:15 -0300

    arrumando settings

--- setup/settings.py
+++ setup/settings.py
@@ -11,19 +11,20 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+from dotenv import load_dotenv
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY='django-insecure-uev(aqr&dgz4r-!l6h2uw*4#hvby_)=ydd3l^gj$7(ria^6w-6'
+SECRET_KEY=os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
